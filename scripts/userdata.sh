410ccb92b78c3be376ae9246ab3c88a4b19d0b0e
Kristian <kristiankassoemeier@gmail.com>
2023-09-21 15:32:35 +0200

    userdata back to commit "before going for s3 copy"

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 sudo yum update -y
 yum update -y
-#Install Apache
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-# Install Git
+# sudo yum update -y
 sudo yum install git -y
-# Install Node.js 16.x LTS
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install 16.0.0
-# Deploy React App
 cd /home/ec2-user
 mkdir "React App"
 cd "React App"
@@ -19,4 +16,3 @@ git clone https://github.com/KristianMeier/cvr-for-aws-september.git .
 npm i
 npm run build
 sudo cp -r build/* /var/www/html/
-# Copy 
